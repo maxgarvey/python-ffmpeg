ed7a9d254e196ce96068bdd4cd68846e8cb89bc4
Max Garvey <maxgarvey@gmail.com>
2016-10-16 14:46:25 -0700

    add to gitignore, and only install ffmpeg once to setup script.

--- setup.sh
+++ setup.sh
@@ -6,10 +6,20 @@ sudo yum update -y
 sudo yum upgrade
 
 # then get python stuff and vim
-sudo yum install vim python python-pip python-virtualenv epel-release wget git gcc yasm
+sudo yum install vim python python-pip python-virtualenv epel-release wget git gcc yasm -y
 
-# get ffmpeg sources from the web
-wget https://ffmpeg.org/releases/ffmpeg-snapshot-git.tar.bz2
+# if ffmpeg command found, then just print 'already installed'
+if which ffmpeg; then
+  printf 'ffmpeg already installed\n'
+else
+  printf 'installing ffmpeg (this may take some time...)\n'
+  # get ffmpeg sources from the web
+  wget https://ffmpeg.org/releases/ffmpeg-snapshot-git.tar.bz2
 
-# untar sources
-tar -xvjf ffmpeg-snapshot-git.tar.bz2
+  # untar sources
+  tar -xvjf ffmpeg-snapshot-git.tar.bz2
+  rm ffmpeg-snapshot-git.tar.bz2
+
+  sudo ffmpeg/configure
+  sudo cd ffmpeg && make install
+fi
