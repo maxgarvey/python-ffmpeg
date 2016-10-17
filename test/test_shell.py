633b39df8afe0dc2890d69dceb87081c2f430915
Max Garvey <maxgarvey@gmail.com>
2016-10-16 18:07:17 -0700

    add negative test in the test_shell file.

--- test/test_shell.py
+++ test/test_shell.py
@@ -4,3 +4,8 @@ def test_run_method():
     return_value, output = run('echo "Hello"')
     assert return_value == 0
     assert output == ['Hello\n']
+
+def test_bad_run_method():
+    return_value, output = run('not-a-real-binary')
+    assert return_value == 127
+    assert output == ['/bin/sh: 1: not-a-real-binary: not found\n']
